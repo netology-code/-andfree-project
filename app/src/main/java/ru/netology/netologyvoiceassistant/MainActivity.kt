d273e580e8edfa1565bdfb9069d2d7f49da2b40f
Nikita Shumskii <lordsxrom@gmail.com>
2021-05-01 10:34:12 +0300

    Урок 1. Задание 2. Вывод переменной на экран

--- app/src/main/java/ru/netology/netologyvoiceassistant/MainActivity.kt
+++ app/src/main/java/ru/netology/netologyvoiceassistant/MainActivity.kt
@@ -2,10 +2,20 @@ package ru.netology.netologyvoiceassistant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myText = "Hello, world!"
+        val myNumber = 42
+        val myFloatingNumber = 3.14
+
+        val outputText = myText + " " + myNumber + " " + myFloatingNumber
+
+        val textView = findViewById<TextView>(R.id.text_output)
+        textView.text = outputText
     }
 }
