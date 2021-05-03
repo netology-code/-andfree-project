6aedaecaa94d52b91cde76498b157d47eb324b4c
Nikita Shumskii <lordsxrom@gmail.com>
2021-05-03 16:33:53 +0300

    Урок 3. Задание 7. Запрос на сервер

--- app/src/main/java/ru/netology/netologyvoiceassistant/MainActivity.kt
+++ app/src/main/java/ru/netology/netologyvoiceassistant/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.widget.ListView
 import android.widget.ProgressBar
 import android.widget.SimpleAdapter
@@ -12,6 +14,11 @@ import androidx.appcompat.widget.Toolbar
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 import com.wolfram.alpha.WAEngine
+import com.wolfram.alpha.WAPlainText
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,12 +28,7 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var podsAdapter: SimpleAdapter
 
-    val pods = mutableListOf<HashMap<String, String>>(
-        HashMap<String, String>().apply {
-            put("Title", "Test title")
-            put("Content", "Test content")
-        }
-    )
+    val pods = mutableListOf<HashMap<String, String>>()
 
     lateinit var progressBar: ProgressBar
 
@@ -45,6 +47,16 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         requestInput = findViewById<TextInputEditText>(R.id.text_input_edit)
+        requestInput.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                pods.clear()
+                podsAdapter.notifyDataSetChanged()
+
+                val question = requestInput.text.toString()
+                askWolfram(question)
+            }
+            return@setOnEditorActionListener false
+        }
 
         val podsList = findViewById<ListView>(R.id.pods_list)
         podsAdapter = SimpleAdapter(
@@ -74,6 +86,53 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun askWolfram(request: String) {
+        progressBar.visibility = View.VISIBLE
+        CoroutineScope(Dispatchers.IO).launch {
+            val query = waEngine.createQuery().apply { input = request }
+            runCatching {
+                waEngine.performQuery(query)
+            }.onSuccess { queryResult ->
+                withContext(Dispatchers.Main) {
+                    progressBar.visibility = View.GONE
+                    if (queryResult.isError) {
+                        showErrorDialog(queryResult.errorMessage)
+                        return@withContext
+                    }
+
+                    if (!queryResult.isSuccess) {
+                        requestInput.error = getString(R.string.error_do_not_understand)
+                        return@withContext
+                    }
+
+                    for (pod in queryResult.pods) {
+                        if (!pod.isError) {
+                            val content = StringBuilder()
+                            for (subpod in pod.subpods) {
+                                for (element in subpod.contents) {
+                                    if (element is WAPlainText) {
+                                        content.append(element.text)
+                                    }
+                                }
+                            }
+                            pods.add(0, HashMap<String, String>().apply {
+                                put("Title", pod.title)
+                                put("Content", content.toString())
+                            })
+                        }
+                    }
+                    podsAdapter.notifyDataSetChanged()
+                }
+            }.onFailure { t ->
+                Log.e(TAG, "Performing Wolfram query was failed", t)
+                withContext(Dispatchers.Main) {
+                    progressBar.visibility = View.GONE
+                    showErrorDialog(t.message ?: getString(R.string.error_something_went_wrong))
+                }
+            }
+        }
+    }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.toolbar_menu, menu)
         return super.onCreateOptionsMenu(menu)
@@ -86,7 +145,9 @@ class MainActivity : AppCompatActivity() {
                 return true
             }
             R.id.action_clear -> {
-                Log.d(TAG, "action_clear")
+                requestInput.text?.clear()
+                pods.clear()
+                podsAdapter.notifyDataSetChanged()
                 return true
             }
             else -> return super.onOptionsItemSelected(item)
