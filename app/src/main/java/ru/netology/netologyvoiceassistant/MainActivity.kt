25acd645c4fcd61b01ba68a21db9db0cef9e0a9a
Nikita Shumskii <lordsxrom@gmail.com>
2021-05-03 15:54:39 +0300

    Урок 2. Задание 4. Тулбар

--- app/src/main/java/ru/netology/netologyvoiceassistant/MainActivity.kt
+++ app/src/main/java/ru/netology/netologyvoiceassistant/MainActivity.kt
@@ -2,28 +2,22 @@ package ru.netology.netologyvoiceassistant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
 
     val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d(TAG, "start of onCreate function")
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val myText = "Hello, world!"
-        val myNumber = 42
-        val myFloatingNumber = 3.14
-
-        val outputText = myText + " " + myNumber + " " + myFloatingNumber
-
-        val textView = findViewById<TextView>(R.id.text_output)
-        textView.text = outputText
-
-        Log.d(TAG, "end of onCreate function")
+        initViews()
     }
+
+    private fun initViews() {
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
 }
