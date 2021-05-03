dab5a0e7470754923733cac28636bc1fde7f8531
Nikita Shumskii <lordsxrom@gmail.com>
2021-05-03 16:00:02 +0300

    Урок 2. Задание 5. Меню

--- app/src/main/java/ru/netology/netologyvoiceassistant/MainActivity.kt
+++ app/src/main/java/ru/netology/netologyvoiceassistant/MainActivity.kt
@@ -2,6 +2,9 @@ package ru.netology.netologyvoiceassistant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +21,25 @@ class MainActivity : AppCompatActivity() {
     private fun initViews() {
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_stop -> {
+                Log.d(TAG, "action_stop")
+                return true
+            }
+            R.id.action_clear -> {
+                Log.d(TAG, "action_clear")
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
     }
 
 }
